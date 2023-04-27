fb5d79e425986c3373e3061cad43955aace96827
ENGE <ae0923068@dt038565.otc.edu>
2023-04-27 09:56:32 -0500

    more stuff

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import chalk from 'chalk';
+import main from './main.js'
+import {configs} from './main.js'
+import axios from 'axios'
 
-console.log(chalk.blue('Hello World'))
+
+// console.log(chalk.blue('Hello World'))
+main()
+configs()
+
+axios.get("https://dog.ceo/api/breeds/image/random?key=587ac573444c34df3cdcc47693dcc377")
+.then (res=>{
+    console.log(res.data)
+})
---
+++ main.js
@@ -0,0 +1,7 @@
+export default function(){
+    console.log("im a default function")
+}
+
+export function configs(){
+    console.log("im the configs function")
+}
